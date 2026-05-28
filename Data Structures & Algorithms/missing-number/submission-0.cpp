9d021fbed545f40728826bd67a1c8c865cdfbe9a
Bhavya Singh <2023kuec2028@iiitkota.ac.in>
2026-05-28 15:30:49 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) 
+    {
+            int ans=0;
+            for(int i=0; i<=nums.size(); i++)
+            {
+                ans ^= i;
+            }
+
+            for(int i=0; i<nums.size(); i++)
+            {
+                ans ^= nums[i];
+            }
+
+            return ans;
+    }
+};
