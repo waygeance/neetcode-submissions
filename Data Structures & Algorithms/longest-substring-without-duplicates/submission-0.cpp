07c7ea97a30492dda1a416cf43a0202b373899b5
Bhavya Singh <2023kuec2028@iiitkota.ac.in>
2026-06-05 04:39:00 +0530

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) 
+    {
+        int i=0;
+        int j=0;
+        int n= s.size();
+        set<char> st;
+        int ans=0;
+
+        while(j<n && i<n)
+        {
+            char ch= s[j];
+
+            if(st.count(ch))
+            {
+                st.erase(st.find(s[i]));
+                i++;
+            }
+
+            else
+            {
+
+                st.insert(ch);
+                ans= max(ans, j-i+1);
+                                j++;
+            }
+        } 
+
+        return ans;   
+    }
+};
+
+// abcadefgh
